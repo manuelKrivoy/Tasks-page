0871db205a5be87289980346777b7295594c1490
Manuel Krivoy <63810676+manuelKrivoy@users.noreply.github.com>
2023-10-09 20:42:12 -0300

    Delete dist directory

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-var _app = _interopRequireDefault(require("./app.js"));
-var _config = require("./config.js");
-_app["default"].listen(_config.PORT);
-console.log("Server Running on PORT ", _config.PORT);
